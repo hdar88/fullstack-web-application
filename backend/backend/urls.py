1e8282dc72d0878392cf7d23abbf44599c01c9b2
hakardar57@gmail.com <hakardar57@gmail.com>
2024-09-14 21:29:29 +0200

    added delete note functionality

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,8 +22,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name = "register"),
-    path("api/token/", TokenObtainPairView.as_view(), name ="get_token" ),
-    path("api/refresh/", TokenRefreshView.as_view(), name ="refresh" ),
+    path("api/token/", TokenObtainPairView.as_view(), name ="get_token"),
+    path("api/refresh/", TokenRefreshView.as_view(), name ="refresh"),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include("api.urls"))
-]
+    path("api/", include("api.urls")),
+    ]
