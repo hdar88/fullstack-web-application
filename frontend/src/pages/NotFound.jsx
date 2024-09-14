0390493646970ddfc2fca1bdb86d329bfcb6272f
hakardar57@gmail.com <hakardar57@gmail.com>
2024-09-14 19:25:19 +0200

    test

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+function NotFound() {
+    return <div>
+        <h1>
+            404 Page not found!
+        </h1>
+        <p>The page does not exist!</p>
+        <p>Possible pages are login, register or home</p>
+    </div>
+}
+
+export default NotFound;
