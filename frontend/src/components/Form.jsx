7b2466fb44d4c792574b858546ead4485735d3a1
hakardar57@gmail.com <hakardar57@gmail.com>
2024-09-15 11:57:22 +0200

    added styling to home dashboard

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,7 +3,7 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css";
-//import LoadingIndicator from "./LoadingIndicator";
+import Loader from "./Loader";
 
 function Form({ route, method }) {
   const [username, setUsername] = useState("");
@@ -50,8 +50,7 @@ function Form({ route, method }) {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
-      {/*
-            {loading && <LoadingIndicator />} */}
+      {loading && <Loader />}
       <button className="form-button" type="submit">
         {name}
       </button>
