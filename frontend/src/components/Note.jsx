36e297742022b3b00957023b5d6915cc0b9f7af7
hakardar57@gmail.com <hakardar57@gmail.com>
2024-09-28 14:10:10 +0200

    add new favorite note tag button

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import "../styles/Note.css";
 
 function Note({ note, onDelete, onEdit }) {
   const formattedDate = new Date(note.created_at).toLocaleDateString("de-DE");
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  // handle favourite tag of note
+  const toggleFavorite = () => {
+    setIsFavorited(!isFavorited);
+  };
 
   return (
     <div className="note-container">
-      <p className="note-title">{note.title}</p>
-      <p className="note-content">{note.content}</p>
+      <div className="note-header">
+        <p className="note-title">{note.title}</p>
+        <button
+          className={`note-fav-button ${isFavorited ? "favorited" : ""}`}
+          onClick={toggleFavorite}
+        >
+          {isFavorited ? "⭐" : "☆"}
+        </button>{" "}
+      </div>
+      <p className="note-label">label tbd</p>
       <p className="note-date">{formattedDate}</p>
       <div className="note-actions">
         <button className="edit-button" onClick={onEdit}>
